efcbae462946cbe5128749adb064bd0b49ea2d56
Marc Oriol Perez <moriolpe@lxplus764.cern.ch>
2023-10-16 20:26:16 +0200

    removed unnecessary code

--- code/main.sh
+++
@@ -1,4 +0,0 @@
-g++ -c -g -Wall `root-config --cflags` plot.cpp
-g++ -c -g -Wall `root-config --cflags` plot_leptons.cpp
-g++ -o plot_leptons.exe `root-config --glibs` plot.o plot_leptons.o
-./plot_leptons.exe
