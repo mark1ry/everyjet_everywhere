a2802b5465fdf39492e3c5080b0bdc7426dca249
Marc Oriol Perez <moriolpe@lxplus786.cern.ch>
2023-10-17 10:57:06 +0200

    added dosGB weighted plot

--- code/main.sh
+++ code/main.sh
@@ -1,4 +1,4 @@
 g++ -c -g -Wall `root-config --cflags` histogram.cpp
-g++ -c -g -Wall `root-config --cflags` plot_single_hist.cpp
-g++ -o plot_single_hist.exe `root-config --glibs` histogram.o plot_single_hist.o
-./plot_single_hist.exe
+g++ -c -g -Wall `root-config --cflags` plot_flavour_hist.cpp
+g++ -o plot_flavour_hist.exe `root-config --glibs` histogram.o plot_flavour_hist.o
+./plot_flavour_hist.exe
