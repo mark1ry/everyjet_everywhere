fbd97b1aaa95818b175152e5efa7bd039fa9c28c
Marc Oriol Perez <moriolpe@lxplus969.cern.ch>
2024-02-02 19:39:05 +0100

    updated comparison

--- klfitter_comparison/compare_dl1d_gn.cpp
+++ klfitter_comparison/compare_dl1d_gn.cpp
@@ -66,108 +66,97 @@ int main () {
     // Set unmatched jets to unmatched value
     Long64_t nentries = nominal->GetEntries();
     int njets = 0;
-    int b_tagged_jets = 0;
-    int n_0_btags = 0;
-    int n_1_btags = 0;
-    int n_2_btags = 0;
-    int n_3_btags = 0;
-    int n_more_btags = 0;
-    unsigned long nw_0_btags = 0;
-    unsigned long nw_1_btags = 0;
-    unsigned long nw_2_btags = 0;
-    unsigned long nw_3_btags = 0;
-    unsigned long nw_more_btags = 0;
-    
-    int Gn_0_btags = 0;
-    int Gn_1_btags = 0;
-    int Gn_2_btags = 0;
-    int Gn_3_btags = 0;
-    int Gn_more_btags = 0;
-    unsigned long Gnw_0_btags = 0;
-    unsigned long Gnw_1_btags = 0;
-    unsigned long Gnw_2_btags = 0;
-    unsigned long Gnw_3_btags = 0;
-    unsigned long Gnw_more_btags = 0;
+    int dl1_b_tagged_jets = 0;
+    int gn_b_tagged_jets = 0;
+    std::vector<int> n_0_btags(5);
+    std::vector<int> n_1_btags(5);
+    std::vector<int> n_2_btags(5);
+    std::vector<int> n_3_btags(5);
+    std::vector<int> n_more_btags(5);
+    std::vector<int> nw_0_btags(5);
+    std::vector<int> nw_1_btags(5);
+    std::vector<int> nw_2_btags(5);
+    std::vector<int> nw_3_btags(5);
+    std::vector<int> nw_more_btags(5);
     
     std::cout << "The total number of events is " << nentries << std::endl;
     bool manual = false;
     
-    int counter = 0;
+    int array_index = 0;
     
     for(Long64_t i=0; i<nentries; i++) {
         if (i==0) { std::cout << "Entered the loop" << std::endl; }
         
         nominal->GetEntry(i);
         updateProgressBar(i+1, nentries);
-        //if (mujets_dl1d_2022 || mujets_2022_dl1d_lowPt || ejets_dl1d_2022 || ejets_2022_dl1d_lowPt) {
-        counter += 1;
+
         njets = nominal_jet_pt->size();
-        b_tagged_jets = 0;
+        dl1_b_tagged_jets = 0;
         for (int j{0}; j<njets; j++) {
             if (manual) {
                 if ((*jet_DL1dv01)[j]>4.854) {
-                    b_tagged_jets += 1;
+                    dl1_b_tagged_jets += 1;
                 }
             } else {
                 if ((*jet_isbtagged_DL1dv01_60)[j]) {
-                    b_tagged_jets += 1;
+                    dl1_b_tagged_jets += 1;
                 }
             }
         }
-        if (b_tagged_jets == 0) {n_0_btags += 1; nw_0_btags += abs(weight);}
-        else if (b_tagged_jets == 1) {n_1_btags += 1; nw_1_btags += abs(weight);}
-        else if (b_tagged_jets == 2) {n_2_btags += 1; nw_2_btags += abs(weight);}
-        else if (b_tagged_jets == 3) {n_3_btags += 1; nw_3_btags += abs(weight);}
-        else {n_more_btags += 1; nw_more_btags += abs(weight);}
         
-        if (b_tagged_jets == 0) {
-            b_tagged_jets = 0;
-            for (int k{0}; k<njets; k++) {
-                if ((*jet_isbtagged_GN120220509_60)[k]) { b_tagged_jets += 1; }
-            }
-            if (b_tagged_jets == 0) {Gn_0_btags += 1; Gnw_0_btags += abs(weight);}
-            else if (b_tagged_jets == 1) {Gn_1_btags += 1; Gnw_1_btags += abs(weight);}
-            else if (b_tagged_jets == 2) {Gn_2_btags += 1; Gnw_2_btags += abs(weight);}
-            else if (b_tagged_jets == 3) {Gn_3_btags += 1; Gnw_3_btags += abs(weight);}
-            else {Gn_more_btags += 1; Gnw_more_btags += abs(weight);}
-            
+        gn_b_tagged_jets = 0;
+        for (int k{0}; k<njets; k++) {
+            if ((*jet_isbtagged_GN120220509_60)[k]) { gn_b_tagged_jets += 1; }
         }
-                
-                
-        //}
+        
+        if (gn_b_tagged_jets == 0) {array_index = 0;}
+        else if (gn_b_tagged_jets == 1) {array_index = 1;}
+        else if (gn_b_tagged_jets == 2) {array_index = 2;}
+        else if (gn_b_tagged_jets == 3) {array_index = 3;}
+        else {array_index = 4;}
+        
+        if (dl1_b_tagged_jets == 0) {n_0_btags[array_index] += 1; nw_0_btags[array_index] += abs(weight);}
+        else if (dl1_b_tagged_jets == 1) {n_1_btags[array_index] += 1; nw_1_btags[array_index] += abs(weight);}
+        else if (dl1_b_tagged_jets == 2) {n_2_btags[array_index] += 1; nw_2_btags[array_index] += abs(weight);}
+        else if (dl1_b_tagged_jets == 3) {n_3_btags[array_index] += 1; nw_3_btags[array_index] += abs(weight);}
+        else {n_more_btags[array_index] += 1; nw_more_btags[array_index] += abs(weight);}
+            
     }
     
-    std::cout << std::endl << "number of selected events found: " << counter << std::endl;
     
     std::cout << std::endl << "B-TAGGED JETS DISTRIBUTION:" << std::endl;
-    std::cout << "0 b-tags:  " << n_0_btags << std::endl;
-    std::cout << "1 b-tag:   " << n_1_btags << std::endl;
-    std::cout << "2 b-tags:  " << n_2_btags << std::endl;
-    std::cout << "3 b-tags:  " << n_3_btags << std::endl;
-    std::cout << "4+ b-tags: " << n_more_btags << std::endl;
+    std::cout << "0 b-tags:  ";
+    for (int index{0}; index<5; index++) {std::cout << n_0_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "1 b-tag:   ";
+    for (int index{0}; index<5; index++) {std::cout << n_1_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "2 b-tags:  ";
+    for (int index{0}; index<5; index++) {std::cout << n_2_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "3 b-tags:  ";
+    for (int index{0}; index<5; index++) {std::cout << n_3_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "4+ b-tags: ";
+    for (int index{0}; index<5; index++) {std::cout << n_more_btags[index] << "  ";}
+    std::cout << std::endl;
     
     std::cout << std::endl << "WEIGHTED B-TAGGED JETS DISTRIBUTION:" << std::endl;
-    std::cout << "0 b-tags:  " << nw_0_btags << std::endl;
-    std::cout << "1 b-tag:   " << nw_1_btags << std::endl;
-    std::cout << "2 b-tags:  " << nw_2_btags << std::endl;
-    std::cout << "3 b-tags:  " << nw_3_btags << std::endl;
-    std::cout << "4+ b-tags: " << nw_more_btags << std::endl;
-    
-    std::cout << std::endl << "EVENTS WITH 0 DL1d B-TAGGINGS:" << std::endl;   
-    
-    std::cout << std::endl << "B-TAGGED JETS DISTRIBUTION:" << std::endl;
-    std::cout << "0 b-tags:  " << Gn_0_btags << std::endl;
-    std::cout << "1 b-tag:   " << Gn_1_btags << std::endl;
-    std::cout << "2 b-tags:  " << Gn_2_btags << std::endl;
-    std::cout << "3 b-tags:  " << Gn_3_btags << std::endl;
-    std::cout << "4+ b-tags: " << Gn_more_btags << std::endl;
-    
-    std::cout << std::endl << "WEIGHTED B-TAGGED JETS DISTRIBUTION:" << std::endl;
-    std::cout << "0 b-tags:  " << Gnw_0_btags << std::endl;
-    std::cout << "1 b-tag:   " << Gnw_1_btags << std::endl;
-    std::cout << "2 b-tags:  " << Gnw_2_btags << std::endl;
-    std::cout << "3 b-tags:  " << Gnw_3_btags << std::endl;
-    std::cout << "4+ b-tags: " << Gnw_more_btags << std::endl;
-    
+    std::cout << "0 b-tags:  ";
+    for (int index{0}; index<5; index++) {std::cout << nw_0_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "1 b-tag:   ";
+    for (int index{0}; index<5; index++) {std::cout << nw_1_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "2 b-tags:  ";
+    for (int index{0}; index<5; index++) {std::cout << nw_2_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "3 b-tags:  ";
+    for (int index{0}; index<5; index++) {std::cout << nw_3_btags[index] << "  ";}
+    std::cout << std::endl;
+    std::cout << "4+ b-tags: ";
+    for (int index{0}; index<5; index++) {std::cout << nw_more_btags[index] << "  ";}
+    std::cout << std::endl;
+
     return 0;
 }
